c8fb7e11f7c9e055b33010fd4356d151866060fc
Ekida <sekidawing@mail.scut.edu.cn>
2016-11-05 12:23:38 +0800

    更新了schedule

--- ScheduleManageServer/ScheduleManageServer/modules/Schedule.js
+++ ScheduleManageServer/ScheduleManageServer/modules/Schedule.js
@@ -6,39 +6,44 @@ function Schedule(schedule) {
     else
         this.schedule_id = schdeule.schedule_id;
     this.name = schedule.name;
+    this.type = schedule.type;
+    this.brief = schedule.brief;
     this.date = new Date(schdeule.date);
     this.title = schedule.title;
     this.span = schedule.span;
     this.details = schedule.details;
-    if (schdeule.edit_time == undefined)
-        this.edit_time = null;
+    if (schdeule.date_set == undefined)
+        this.date_set = null;
     else
-        this.edit_time = schedule.edit_time;
+        this.date_set = schedule.date_set;
 };
 
 module.exports = Schedule;
 
-Schedule.prototype.toJSON = function save(callback) {
+Schedule.prototype.toJSON = function toJSON() {
     var schedule = {
         schedule_id: this.schedule_id,
         name: this.name,
+        type: this.type,
+        brief: this.brief,
         date: this.date.toJSON(),
         title: this.title,
         span: this.span,
         details: this.details,
-        edit_time = this.edit_time
+        date_set: this.date_set
     };
+    return schedule;
 }
 
 Schedule.prototype.save = function save(callback) {
-    this.date = new Date();
+    this.date_set = new Date();
     db.insert('schedules', null, schedule, function (err, result) {
         this.schedule_id = result.insertedId;
         callback(err);
     });
 }
 
-Schedule.get = function (id, callback) {
+Schedule.get = function get(id, callback) {
     db.get('schedules', {schedule_id: id}, function (err, result) {
         if (result) {
             var newSchedule = new Schedule(result);
@@ -51,8 +56,19 @@ Schedule.get = function (id, callback) {
 }
 
 Schedule.prototype.update = function update(callback) {
+    this.date_set = new Date();
     var s = this.toJSON();
     var id = s.schedule_id;
     delete s.schedule_id;
     dbControl.update('schedules', { _id: id }, s, callback);
 }
+
+Schedule.findBySelector = function findBySelector(selector, callback) {
+    db.findBySelector('schedules', selector, function (err, result) {
+        var schedules = new Array(result.length);
+        for (var i = 0; i < schedules.length; i++)
+            schedules[i] = new Schedule(result[i]);
+        callback(err, schedules);
+    });
+}
+
